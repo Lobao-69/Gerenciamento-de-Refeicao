983713adc0b5e5e317b6346816d82df26fd0191e
todpig <luccasraffael6@gmail.com>
2023-09-07 12:17:02 -0300

    fazendo login com google

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
+from .views import *
 
 urlpatterns = [
-    
+    path("", IndexView.as_view(), name="index"),
+    path("profile/", ProfileView.as_view(), name="profile"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
 
-# Create your views here.
+class IndexView(TemplateView):
+    template_name = "index.html"
+
+class ProfileView(TemplateView):
+    template_name = "profile.html"
